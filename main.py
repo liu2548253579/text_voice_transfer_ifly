76d9c33e753ded58317f34ff290947e3754ba94f
Frank Liu <2548253579@qq.com>
2023-04-21 14:01:49 +0800

    initialize file

---
+++ main.py
@@ -0,0 +1,15 @@
+from record_voice import record
+from audio_play import play
+from ifly_t2a import text_to_audio
+from ifly_a2t import audio_to_text
+
+file = 'test.wav'           # 语音录制，识别文件
+synth_file = "synth.mp3"    # 语音合成文件 
+
+record(file)                # 录制音频 
+txt_str = audio_to_text(file)               # 语音识别
+print(txt_str)                              # 打印识别结果
+
+ret = text_to_audio(synth_file,txt_str)    # 语音合成
+if ret == 1:
+    play(synth_file)                        # 播放合成结果
---
+++ readme.md
@@ -0,0 +1,35 @@
+# 语音转文字与文字转语音实现
+
+## 1.安装
+
+确保你所处的操作系统已安装ffmpeg
+
+然后cd到工程目录下
+
+安装所需依赖：
+
+```bash
+pip install -r requirements.txt
+```
+
+## 2.配置科大讯飞API
+
+去[科大讯飞开放平台官网](https://www.xfyun.cn/)主策并申请语音识别的API并免费领取在线文字转语音和语音转文字次数后,获取到**APP_ID、API_KEY**与**SECRET_KEY**并将其填写到**ifly_api.txt**
+
+例如：
+
+```bash
+APP_ID:123456789
+API_KEY:xxxxxxxxxxxxxxxxxxx
+SECRET_KEY:xxxxxxxxxxxxxxx
+```
+
+## 3.开始使用
+
+打开终端输入如下指令即可开始使用
+
+```
+python main.py
+```
+
+根据提示可以录入声音并转化为文字，而后将文字转化为合成语音。

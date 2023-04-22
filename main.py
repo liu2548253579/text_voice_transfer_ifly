f6c58b9e7b4c787f3936677a3cb890f4f0e61a9c
Frank Liu <2548253579@qq.com>
2023-04-22 13:48:29 +0800

    update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from record_voice import record
-from audio_play import play
-from ifly_t2a import text_to_audio
-from ifly_a2t import audio_to_text,clear_text
+from ifly.record_voice import record
+from ifly.audio_play import play
+from ifly.ifly_t2a import text_to_audio
+from ifly.ifly_a2t import audio_to_text,clear_text
 
 file = 'user_voice.wav'           # 语音录制，识别文件
 synth_file = "generated_voice.mp3"    # 语音合成文件 

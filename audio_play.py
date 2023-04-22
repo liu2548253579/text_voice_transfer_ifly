54d443da341936052dcff6071c492a595d31c2f8
Frank Liu <2548253579@qq.com>
2023-04-22 14:11:11 +0800

    fix log too much

--- audio_play.py
+++ audio_play.py
@@ -2,5 +2,5 @@ import os
 
 def play(file_name):
     """audio play"""
-    os.system(f"ffplay -autoexit {file_name}")		#播放完毕自动退出
+    os.system(f"ffplay -autoexit -hide_banner {file_name}")		#播放完毕自动退出
 
